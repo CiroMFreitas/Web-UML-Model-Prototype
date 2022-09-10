8fa26b79c75525a97521bdc2cda184f6f9bc0ec3
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-09-10 16:20:19 -0300

    alter command now changes models id and name on tittle

--- src/ClassDiagramCreator.js
+++ src/ClassDiagramCreator.js
@@ -1,10 +1,11 @@
 //Errors
 const UNDEFINED_COMMAND = "Undefined command!";
 const CREATE_ERROR = "Create must have at minimum type and name!";
-const ALTER_ERROR = "Create must have at minimum type, name and one argument!";
-const TYPE_UNDEFINED = "Specified type is undefined!"
+const ALTER_ERROR = "Alter must have at minimum type, name and one argument!";
+const TYPE_UNDEFINED = "Specified type is undefined!";
 const CLASS_NAME_ERROR = "Name used is invalid or missing!";
-const ARGUMENTS_ERROR = "Arguments are missing or invalid!"
+const ARGUMENTS_ERROR = "Arguments are missing or invalid!";
+const MODEL_NOT_FOUND = "Required model was not found!";
 
 $(document).ready(function() {
     //Key's codes
@@ -122,19 +123,24 @@ function createCommandHandler(creationArguments) {
 }
 
 //Alteration handlers
-function alterCommandHandler(creationArguments) {
+function alterCommandHandler(alterationArguments) {
     try {
         //Validade minimum arguments
-        if(creationArguments.length < 4) {
+        if(alterationArguments.length < 5) {
             throw ALTER_ERROR;
         }
 
         //Type name must exist and be alphabetic character only
-        typeValidationHandler(creationArguments[1]);
+        typeValidationHandler(alterationArguments[1]);
 
         //Class name must be alphabetic character only
-        nameValidationHandler(creationArguments[2]);
-        const className = creationArguments[2];
+        nameValidationHandler(alterationArguments[2]);
+        var className = alterationArguments[2]; //Create Class table
+
+        const newName = modelNameChangeHandler(alterationArguments, className);
+        if (newName) {
+            className = newName;
+        }
     } catch (error) {
         insertIntoCommandHistory(error)
     }
@@ -231,4 +237,71 @@ function nameValidationHandler(name) {
     if(!(/^[A-Za-z]*$/.test(name))) {
         throw CLASS_NAME_ERROR;
     }
+}
+
+//Model name change
+function modelNameChangeHandler(commandArguments, className) {
+    //Checks if model exists
+    const model = $(`#${className}Class`);
+    if(model.length == 0) {
+        throw MODEL_NOT_FOUND;
+    }
+
+    //Checks if name is to be changed and valid
+    const nameChangeArgument = commandArguments.indexOf("-n");
+    if(nameChangeArgument == -1) {
+        return false;
+    }
+    const newName = commandArguments[nameChangeArgument + 1].trim();
+    nameValidationHandler(newName);
+
+    //Changes base model html ids to new name format
+    model.attr("id", `#${newName}Class`);
+    model.children()[0].id = newName + "ClassNameRow";
+    model.children()[1].id = newName + "ClassAttributesRow";
+    model.children()[2].id = newName + "ClassMethodsRow";
+
+    //Changes model tittle/name row to new name format
+    const modelNameRow = $(`#${newName}ClassNameRow`);
+    modelNameRow.children()[0].id = newName + "ClassTitle";
+    modelNameRow.children()[1].id = newName + "ClassName";
+    modelNameRow.children()[1].innerText = newName;
+
+    //Changes model attribute row to new name format
+    const modelAttributeRow = $(`#${newName}ClassAttributesRow`);
+    const attributes = modelAttributeRow.children();
+    attributes[0].id = newName + "ClassAttributeTitle";
+    if(attributes[1].innerText != "-") {
+        for(let i = 1; i < attributes.length; i++) {
+            const attribute = attributes[i].children;
+            const attributeName = attribute[2].innerText;
+    
+            attributes[i].id = newName + "Class" + attributeName + "AttributeRow";
+            attribute[0].id = newName + "Class" + attributeName + "AttributeVisibility";
+            attribute[1].id = newName + "Class" + attributeName + "AttributeType";
+            attribute[2].id = newName + "Class" + attributeName + "AttributeName";
+        }
+    } else {
+        attributes[1].id = newName + "ClassEmptyAttributeRow";
+    }
+
+    //Changes model methods row to new name format
+    const modelMethodRow = $(`#${newName}ClassMethodsRow`);
+    const methods = modelMethodRow.children();
+    methods[0].id = newName + "ClassMethodTitle";
+    if(methods[1].innerText != "-") {
+        for(let i = 1; i < methods.length; i++) {
+            const method = methods[i].children;
+            const methodName = method[2].innerText;
+    
+            methods[i].id = newName + "Class" + methodName + "MethodRow";
+            method[0].id = newName + "Class" + methodName + "MethodVisibility";
+            method[1].id = newName + "Class" + methodName + "MethodType";
+            method[2].id = newName + "Class" + methodName + "MethodName";
+        }
+    } else {
+        methods[1].id = newName + "ClassEmptyMethodRow";
+    }
+
+    return newName;
 }
