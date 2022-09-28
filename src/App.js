82f25439df6ba76690975fc39a3919a02a7a728a
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-09-28 14:13:17 -0300

    hardcoded command history insert

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ClassDiagramCanvas from "./Components/ClassDiagramCanvas/ClassDiagramCanvas";
 import CommandPanel from "./Components/CommandPanel/CommandPanel";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [commands, setCommands] = useState([]);
+
+  useEffect(() => {
+    setCommands(prevCommands => {
+      return [prevCommands,
+        { id: uuidv4(), line: "Command 1" },
+        { id: uuidv4(), line: "Command 2" }]
+    });
+  }, [])
 
   return (
     <>
--- src/Components/CommandPanel/CommandPanel.js
+++ src/Components/CommandPanel/CommandPanel.js
@@ -6,9 +6,14 @@ export default function CommandPanel({ commands }) {
   return (
     <div id="CommandPanel">
       <div id="CommandHistory" aria-live="polite" disabled>
+        {
+          commands.map(command => {
+            return <CommandLine key={ command.id } command={ command.line } />
+          })
+        }
       </div>
       
-      <textarea id="CommandConsole" aria-label="Console de comandos" autofocus></textarea>
+      <textarea id="CommandConsole" aria-label="Console de comandos" autoFocus></textarea>
     </div>
   );
 }
