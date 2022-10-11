dfa02f3a5766a97025bcd6e17b8aa2c57e61d966
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-10-11 16:15:06 -0300

    created method getLastArgumentIndexHandler

--- src/Components/Handlers/UtilityHandlers/DataHandler.js
+++ src/Components/Handlers/UtilityHandlers/DataHandler.js
@@ -1,3 +1,6 @@
+// Errors
+const ERROR_MISSING_END_SYMBOL = "Um dos argumentos do comando não foi fechado apropriadamente!";
+
 /**
  * Returns top most key which has the searched value, the search will go deep if object contains another object within, but will use includes function on arrays.
  * 
@@ -23,4 +26,20 @@ export function getKeyByValue(object, value) {
     })
 
     return false;
+}
+
+/**
+ * Returns the index of a arguments array which it's last charcther is the expressed symbol, if smbol is missing a error is thrown.
+ * 
+ * @param {String[]} argumentsArray 
+ * @param {String} endSymbol 
+ */
+export function getLastArgumentIndexHandler(argumentsArray, endSymbol) {
+    const lastArgument = argumentsArray.find((argument) => argument.includes(endSymbol))
+    
+    if(lastArgument) {
+        return argumentsArray.indexOf(lastArgument);
+    } else {
+        throw ERROR_MISSING_END_SYMBOL;
+    }
 }
