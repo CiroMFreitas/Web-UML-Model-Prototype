5b626736840f76719a743baeeac994efc04c5d6a
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-10-11 12:21:46 -0300

    added in code documentation for upperCaseFirstLetter

--- src/Components/Handlers/UtilityHandlers/StringHandler.js
+++ src/Components/Handlers/UtilityHandlers/StringHandler.js
@@ -1,3 +1,8 @@
+/**
+ * Receives a string and returns the same string with it's first letter upper cased.
+ * 
+ * @param {String} upperCaseString
+ */
 export function upperCaseFirstLetter(upperCaseString) {
     const firstLetter = upperCaseString.charAt(0).toUpperCase();
 
