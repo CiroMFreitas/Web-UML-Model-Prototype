f2d3a4c5dc73423048968fca68d102208f28c5bd
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-10-11 11:31:10 -0300

    refactoring getKeyByValue and added in code documentation

--- src/Components/Handlers/DataHandler.js
+++ src/Components/Handlers/DataHandler.js
@@ -1,22 +1,26 @@
-export function getKeyByValue(jsonObject, value) {
-    let lookUpKey = "";
-
-    Object.keys(jsonObject).forEach((key) => {
+/**
+ * Returns top most key which has the searched value, the search will go deep if object contains another object within, but will use includes function on arrays.
+ * 
+ * If no key is found a boolean false will be returned.
+ * 
+ * @param {Object} object 
+ * @param {String} value 
+ */
+export function getKeyByValue(object, value) {
+    Object.keys(object).forEach((key) => {
         if(key === value) {
-            lookUpKey = key;
+            return key;
         } else if(Array.isArray(key)) {
-            if(isValueInArray(jsonObject[key], value)) {
-                lookUpKey = key;
+            if(object[key].includes(value)) {
+                return key;
             }
-        } else if(JSON.constructor === jsonObject.constructor) {
-            const keyInObject = getKeyByValue(jsonObject[key], value);
-            if(keyInObject !== "") {
-                lookUpKey = key;
+        } else if(JSON.constructor === object.constructor) {
+            const keyInObject = getKeyByValue(object[key], value);
+            if(keyInObject) {
+                return key;
             }
         }
     })
 
-    return lookUpKey;
-}
-
+    return false
 }
