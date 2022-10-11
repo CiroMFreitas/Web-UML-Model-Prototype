6d57b4141cdbfac6c00aaca985d930681987fa2e
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-10-11 11:59:06 -0300

    refactored ClassDiagramCommandHandler to delegate entity generation to more specialized functions

--- src/Components/Handlers/ClassDiagramCommandsHandler.js
+++ src/Components/Handlers/ClassDiagramCommandsHandler.js
@@ -1,102 +1,64 @@
 import { v4 as uuidv4 } from "uuid";
-import { upperCaseFirstLetter } from "./StringHandler";
+import { upperCaseFirstLetter } from "./UtilityHandlers/StringHandler";
+import addClassCommandHandler from "./ClassEnityHandlers/AddClassCommandHandler";
 
 // Supported Key Words
-const SUPPORTED_ENTITY_TYPES = {
-    classType: "classe"
-};
+const SUPPORTED_COMMANDS = {
+    add: [
+        "add",
+        "adicionar"
+    ]
+}
 
-const SUPPORTED_VISIBILITY = [
-    "pública",
-    "protegida",
-    "privada"
-];
+const SUPPORTED_ENTITY_TYPES = {
+    classType: [
+        "class",
+        "classe"
+    ]
+};
 
 // ERRORS
 const ERROR_UNRECOGNISED_COMMAND = "Comando não reconhecido!";
 const ERROR_UNRECOGNISED_ENTITY_TYPE = "Entidade não reconhecida!";
-const ERROR_COMMAND_SYNTAX = "Erro de sintaxe no commando!";
 
+/**
+ * Receives a command line and handles it into an object to be stored and rendered on application.
+ * 
+ * @param {String} commandLine
+ */
 export default function classDiagramCommandsHandler(commandLine) {
     const command = commandLine.replace("\n", "").split(" ");
-    let handledCommandLine;
+    const handledObjectEntity = {
+        id: uuidv4()
+    };
 
-    if(!SUPPORTED_ENTITY_TYPES.classType === command[1]) {
-        throw ERROR_UNRECOGNISED_ENTITY_TYPE;
-    }
-
-    switch(command[0].toLowerCase()) {
-        case "adicionar":
-            handledCommandLine = addCommandHandler(command);
+    switch(true) {
+        case SUPPORTED_COMMANDS.add.includes(command[0].toLowerCase()):
+            Object.assign(handledObjectEntity, addCommandHandler(command));
             break;
 
         default:
             throw ERROR_UNRECOGNISED_COMMAND;
     }
 
-    return handledCommandLine;
+    return handledObjectEntity;
 }
 
+// Check which type is to be handled
 function addCommandHandler(command) {
-    const handledAddCommand = {
-        id: uuidv4(),
+    const handledAddEntity = {
         type: "add",
         entityType: upperCaseFirstLetter(command[1])
     }
-
-    switch(command[1].toLowerCase()) {
-        case SUPPORTED_ENTITY_TYPES.classType:
-            handledAddCommand["entityName"] = upperCaseFirstLetter(command[2]);
-
-            // Possible Arguments
-            handledAddCommand["attributes"] = [];
-            if(command.indexOf("-a") !== -1) {
-                handledAddCommand.attributes = addArgumentsHandler(command, command.indexOf("-a"));
-            }
-            handledAddCommand["methods"] = [];
-            if(command.indexOf("-m") !== -1) {
-                handledAddCommand.methods = addArgumentsHandler(command, command.indexOf("-m"));
-            }
+    
+    switch(true) {
+        case SUPPORTED_ENTITY_TYPES.classType.includes(handledAddEntity.entityType.toLowerCase()):
+            Object.assign(handledAddEntity, addClassCommandHandler(command));
             break;
             
         default:
-            throw ERROR_COMMAND_SYNTAX;
+            throw ERROR_UNRECOGNISED_ENTITY_TYPE;
     }
 
-    return handledAddCommand;
-}
-
-function addArgumentsHandler(command, argumentIndex) {
-    const addArguments = []
-
-    for(let i = argumentIndex + 1; command[i] && command[i].includes(":"); i++) {
-        const addArgument = command[i].split(":");
-        
-        switch(addArgument.length) {
-            case 3:
-                if(SUPPORTED_VISIBILITY.includes(addArgument[0])) {
-                    addArguments.push({
-                        visibility: addArgument[0],
-                        type: addArgument[1],
-                        name: addArgument[2]
-                    });
-                } else {
-                    throw ERROR_COMMAND_SYNTAX;
-                }
-                break;
-
-            case 2:
-                addArguments.arguments.push({
-                    visibility: SUPPORTED_VISIBILITY[0],
-                    type: addArgument[0],
-                    name: addArgument[1]
-                });
-                break;
-            
-            default:
-                throw ERROR_COMMAND_SYNTAX;
-        }
-    }
-
-    return addArguments;
+    return handledAddEntity;
 }
