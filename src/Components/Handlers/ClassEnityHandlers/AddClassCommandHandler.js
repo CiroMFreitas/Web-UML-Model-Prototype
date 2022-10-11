baada3d018bfcc7f9af2b8a9ea1502eab2374881
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-10-11 16:23:41 -0300

    addClassCommandHandler classe name handling

--- src/Components/Handlers/ClassEnityHandlers/AddClassCommandHandler.js
+++ src/Components/Handlers/ClassEnityHandlers/AddClassCommandHandler.js
@@ -1,3 +1,59 @@
+import { upperCaseFirstLetter, validateNameSpace } from "../UtilityHandlers/StringHandler";
+
+// Supported Key Words
+const SUPPORTED_VISIBILITY = {
+    public: [
+        "public",
+        "pública"
+    ],
+    protected: [
+        "protected",
+        "protegida"
+    ],
+    private: [
+        "private",
+        "privada"
+    ]
+};
+
+// Errors
+const ERROR_COMMAND_SYNTAX = "Erro na sintaxe do comando!"
+
+/**
+ * Handles command returning an object to be used as a classEntity.
+ * 
+ * @param {String} command 
+ */
 export default function addClassCommandHandler(command) {
-    return {};
+    const handledAddEntity = {
+        entityName: upperCaseFirstLetter(validateNameSpace(command[2])),
+        attributes: [],
+        methods: []
+    }
+
+    // Get attributes and methods
+    if(command.indexOf("-a") !== -1) {
+        const firstAttributeArgumentIndex = command.indexOf("-a") + 1;
+
+        handledAddEntity.attributes = addAttributesHandler(command.slice(firstAttributeArgumentIndex));
+    }
+    
+    if(command.indexOf("-m") !== -1) {
+        handledAddEntity.methods = addMethodsHandler(command, command.indexOf("-m") + 1);
+    }
+
+    return handledAddEntity;
+}
+
+// Handles possible attributes
+function addAttributesHandler(attributesArguments) {
+    const addAttributes = []
+
+    return addAttributes;
+}
+// Handles possible methods
+function addMethodsHandler(methodsArguments) {
+    const addMethods = [];
+
+    return addMethods;
 }
