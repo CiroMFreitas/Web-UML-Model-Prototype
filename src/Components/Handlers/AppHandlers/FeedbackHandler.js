7729abb543ff039a2f71cf1e292a9273e419d79e
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-10-14 14:48:25 -0300

    class creation sends proper feedback now

--- src/Components/Handlers/AppHandlers/FeedbackHandler.js
+++ src/Components/Handlers/AppHandlers/FeedbackHandler.js
@@ -1,5 +1,28 @@
+import { ERROR_FAILED_FEEDBACK_HANDLING } from "../../../Utils/Errors";
+import { SUPPORTED_COMMANDS, SUPPORTED_ENTITY_TYPES } from "../../../Utils/SupportedKeyWords";
+
 export default function feedbackHandler(handledCommand) {
-    let handledFeedback = "mock";
-    
+    let handledFeedback;
+
+    switch(true) {
+        case SUPPORTED_COMMANDS.create.includes(handledCommand.command):
+            handledFeedback = createFeedBackHandler(handledCommand);
+
+        break;
+
+        default:
+            return ERROR_FAILED_FEEDBACK_HANDLING;
+    }
+
     return handledFeedback;
+}
+
+function createFeedBackHandler(createdEntity) {
+    switch(true) {
+        case SUPPORTED_ENTITY_TYPES.class.includes(createdEntity.entityType.toLowerCase()):
+            return "A classe " + createdEntity.entityName + " foi criada com sucesso!";
+
+        default:
+            return ERROR_FAILED_FEEDBACK_HANDLING;
+    }
 }
