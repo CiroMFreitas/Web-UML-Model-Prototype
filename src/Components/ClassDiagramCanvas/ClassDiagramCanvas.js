e7352b76bba83899cbbcb329f4692fed80610695
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-10-14 10:51:04 -0300

    refactor and temporary comment when printing handledClassEntity for the application

--- src/Components/ClassDiagramCanvas/ClassDiagramCanvas.js
+++ src/Components/ClassDiagramCanvas/ClassDiagramCanvas.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import CommandHandlerContext from '../../Contexts/CommandHandlerContext';
 import ClassEntity from '../ClassEntity/ClassEntity';
 import "./ClassDiagramCanvas.css";
 
 export default function ClassDiagramCanvas() {
+  const { classEntities } = useContext(CommandHandlerContext);
 
   // Component
   return (
     <div id="ClassDiagramCanvas">
-      <ClassEntity />
+      {
+        classEntities.map(classEntity => {
+          return <ClassEntity key={ classEntity.id } type={ classEntity.entityType } name={ classEntity.entityName } attributes={ classEntity.attributes } methods={ classEntity.attributes } />
+        })
+      }
     </div>
   );
 }
--- src/Components/ClassEntity/ClassEntity.js
+++ src/Components/ClassEntity/ClassEntity.js
@@ -1,67 +1,74 @@
-import React, { useContext } from 'react';
-import CommandHandlerContext from '../../Contexts/CommandHandlerContext';
+import React from 'react';
 
-export default function ClassEntity() {
-    const { classEntities } = useContext(CommandHandlerContext);
+export default function ClassEntity(type, name, attributes, methods) {
 
-    function renderAttribute(classEntity) {
-        if(classEntity.attributes.length === 0) {
-          return (
-            <div className="emptyLine">-</div>
+  /*
+  function renderAttribute() {
+      if(attributes.length === 0) {
+        return (
+          <div className="emptyLine">-</div>
+        );
+      } else {
+          return(
+              attributes.map((attribute) => (
+                  <div className="attributeLine">
+                    <div className="attributeVisibility">{ attribute.visibility }</div>
+                    <div className="attributeType">{ attribute.type }</div>
+                    <div className="attributeName">{ attribute.name }</div>
+                  </div>
+              ))
           );
-        } else {
-            return(
-                classEntity.attributes.map((attribute) => (
-                    <div className="attributeLine">
-                      <div className="attributeVisibility">{ attribute.visibility }</div>
-                      <div className="attributeType">{ attribute.type }</div>
-                      <div className="attributeName">{ attribute.name }</div>
-                    </div>
-                ))
-            );
-        }
-    }
+      }
+  }
 
-    function renderMethod(classEntity) {
-        if(classEntity.methods.length === 0) {
-          return (
-            <div className="emptyLine">-</div>
-          );
-        } else {
-          return (
-            classEntity.methods.map((method) => (
-                <div className="methodLine">
-                  <div className="methodVisibility">{ method.visibility }</div>
-                  <div className="methodType">{ method.type }</div>
-                  <div className="methodName">{ method.name }</div>
+  function renderMethod() {
+      if(methods.length === 0) {
+        return (
+          <div className="emptyLine">-</div>
+        );
+      } else {
+        return (
+          methods.map((method) => (
+              <div className="methodLine">
+                <div className="methodVisibility">{ method.visibility }</div>
+                <div className="methodType">{ method.type }</div>
+                <div className="methodName">{ method.name }</div>
+                <div className="methodParameters">(
+                  {
+                    "(" + method.parameters.map((parameter, i, arr) => (
+                      parameter.type + ": " +  parameter.name + (arr.length - 1 !== i ? ", " : ")")
+                    ))
+                  }
                 </div>
-            ))
-          );
-        }
-    }
-    
-    return (
-        <>
-        {
-            classEntities.map((classEntity) => (
-                <div className="classEntity" key={ classEntity.id }>
-                    <div className="classTittle">
-                        <div className="entityType">{ classEntity.entityType }</div>
-                        <div className="entityName">{ classEntity.entityName }</div>
-                    </div>
-                    
-                    <div className="EntityAttributes">
-                      <div className="attributesTittle">Atributos</div>
-                      { renderAttribute(classEntity) }
-                    </div>
+              </div>
+          ))
+        );
+      }
+  }
+  */
 
-                        <div className="EntityMethods">
-                          <div className="methodsTittle">Métodos</div>
-                          { renderMethod(classEntity) }
-                        </div>
-                    </div>
-                ))
-            }
-        </>
-    )
+  return (
+    <div className="classEntity">
+      <div className="classTittle">
+          <div className="entityType">{ type }</div>
+          <div className="entityName">{ name }</div>
+      </div>
+      { console.log(type) }
+      { console.log(name) }
+      { console.log(attributes) }
+      { console.log(methods) }
+    </div>
+  )
 }
+
+/*           
+  <div className="EntityAttributes">
+    <div className="attributesTittle">Atributos</div>=
+    { renderAttribute() }
+  </div>
+
+  <div className="EntityMethods">
+    <div className="methodsTittle">Métodos</div>
+    { renderMethod() }
+  </div>
+*/
