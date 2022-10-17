b484601d92cccee9120cf15f38bb48519420cb90
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-10-17 11:07:51 -0300

    it is now possible to change a existing class name

--- src/Handlers/ClassEnityHandlers/AlterClassCommandHandler.js
+++ src/Handlers/ClassEnityHandlers/AlterClassCommandHandler.js
@@ -1,6 +1,21 @@
+import { upperCaseFirstLetter, validateNameSpace } from "../UtilityHandlers/StringHandler";
+
 export default function alterClassCommandHandler(commandArray, alteringClass) {
     console.log(commandArray);
     console.log(alteringClass);
 
-    return { };
+    const handledAlteringClass = alteringClass;
+
+    const renameIndex = commandArray.indexOf("-n");
+    if(renameIndex) {
+        handledAlteringClass.entityName = alterNameHandler(commandArray[renameIndex + 1]);
+    }
+
+    return handledAlteringClass;
+}
+
+function alterNameHandler(oldName) {
+    const newName = upperCaseFirstLetter(validateNameSpace(oldName.toLowerCase()));
+
+    return newName;
 }
