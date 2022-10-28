dbab3f5f23495ed62a1e75d427e4ef27f7a4d739
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-10-28 09:02:28 -0300

    revised stringHandler documentation

--- src/Handlers/UtilityHandlers/StringHandler.js
+++ src/Handlers/UtilityHandlers/StringHandler.js
@@ -3,18 +3,22 @@ import { ERROR_INVALID_CHARACTER } from "../../Utils/Errors";
 /**
  * Receives a string and returns the same string with it's first letter upper cased.
  * 
- * @param {String} upperCaseString
+ * @param {string} toUpperCaseString
+ * 
+ * @return {string} upperCasedString
  */
-export function upperCaseFirstLetter(upperCaseString) {
-    const firstLetter = upperCaseString.charAt(0).toUpperCase();
+export function upperCaseFirstLetter(toUpperCaseString) {
+    const firstLetter = toUpperCaseString.charAt(0).toUpperCase();
 
-    return firstLetter + upperCaseString.substring(1)
+    return firstLetter + toUpperCaseString.substring(1)
 }
 
 /**
  * Checks if string's characthers are alphanumeric only, if true returns namespace, else throws error.
  * 
- * @param {String} nameSpace
+ * @param {string} nameSpace
+ * 
+ * @return {string} nameSpace
  */
 export function validateNameSpace(nameSpace) {
     if(!(/^[a-z0-9]+$/i.test(nameSpace))) {
