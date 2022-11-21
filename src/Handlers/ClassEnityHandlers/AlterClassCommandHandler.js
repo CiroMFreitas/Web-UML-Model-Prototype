4f61d66317cfb8114400ce78d6c5cf5cd5b989bf
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-11-21 10:51:53 -0300

    renaming variables in order to avoid confusion

--- src/Handlers/ClassEnityHandlers/AlterClassCommandHandler.js
+++ src/Handlers/ClassEnityHandlers/AlterClassCommandHandler.js
@@ -1,6 +1,6 @@
 import { ERROR_COMMAND_SYNTAX } from "../../Utils/Errors";
 import { SUPPORTED_ALTER_ARGUMENTS, SUPPORTED_VISIBILITY } from "../../Utils/SupportedKeyWords";
-import { attributesFormatter, getKeyByValue } from "../UtilityHandlers/DataHandler";
+import { attributesFormatter, getKeyByValue, methodsFormatter } from "../UtilityHandlers/DataHandler";
 import { upperCaseFirstLetter, validateNameSpace } from "../UtilityHandlers/StringHandler";
 
 export default function alterClassCommandHandler(commandArray, alteringClass, renameIndex) {
@@ -12,7 +12,12 @@ export default function alterClassCommandHandler(commandArray, alteringClass, re
 
     const alteringAttributesIndex = commandArray.indexOf("-a");
     if(alteringAttributesIndex !== -1) {
-        alterAttributesHandler(commandArray.splice(alteringAttributesIndex), alteringClass)
+        alterAttributesHandler(commandArray.slice(alteringAttributesIndex), alteringClass)
+    }
+
+    const alteringMethodsIndex = commandArray.indexOf("-m");
+    if(alteringMethodsIndex !== -1) {
+        alterMethodsHandler(commandArray.slice(alteringMethodsIndex), alteringClass)
     }
 
     return handledAlteringClass;
@@ -23,20 +28,20 @@ function alterNameHandler(newName) {
 }
 
 function alterAttributesHandler(alteringArguments, alteringClass) {
-    const alteringAttributes = attributesFormatter(alteringArguments);
+    const formattedArguments = attributesFormatter(alteringArguments);
     
-    alteringAttributes.forEach((alteringAttribute) => {
-        const argument = alteringAttribute.shift().toLowerCase();
+    formattedArguments.forEach((formattedArgument) => {
+        const argument = formattedArgument.shift().toLowerCase();
 
         switch(true) {
             case SUPPORTED_ALTER_ARGUMENTS.add.includes(argument):
-                switch(alteringAttribute.length) {
+                switch(formattedArgument.length) {
                     case 3:
-                        if(getKeyByValue(SUPPORTED_VISIBILITY, alteringAttribute[0])) {
+                        if(getKeyByValue(SUPPORTED_VISIBILITY, formattedArgument[0])) {
                             alteringClass.attributes.push({
-                                visibility: alteringAttribute[0],
-                                type: validateNameSpace(alteringAttribute[1]),
-                                name: validateNameSpace(alteringAttribute[2])
+                                visibility: formattedArgument[0],
+                                type: validateNameSpace(formattedArgument[1]),
+                                name: validateNameSpace(formattedArgument[2])
                             });
                         } else {
                             throw ERROR_COMMAND_SYNTAX;
@@ -46,8 +51,8 @@ function alterAttributesHandler(alteringArguments, alteringClass) {
                     case 2:
                         alteringClass.attributes.push({
                             visibility: SUPPORTED_VISIBILITY.public[1],
-                            type: validateNameSpace(alteringAttribute[0]),
-                            name: validateNameSpace(alteringAttribute[1])
+                            type: validateNameSpace(formattedArgument[0]),
+                            name: validateNameSpace(formattedArgument[1])
                         });
                         break;
                     
@@ -57,7 +62,7 @@ function alterAttributesHandler(alteringArguments, alteringClass) {
             break
 
             case SUPPORTED_ALTER_ARGUMENTS.remove.includes(argument):
-                const removingAttibuteIndex = alteringClass.attributes.findIndex((attribute) => alteringAttribute[0] === attribute.name);
+                const removingAttibuteIndex = alteringClass.attributes.findIndex((attribute) => formattedArgument[0] === attribute.name);
 
                 if(removingAttibuteIndex !== -1) {
                     alteringClass.attributes.splice(removingAttibuteIndex, 1);
@@ -68,44 +73,49 @@ function alterAttributesHandler(alteringArguments, alteringClass) {
                 break;
 
             case SUPPORTED_ALTER_ARGUMENTS.alter.includes(argument):
-                const alteringAttributename = alteringAttribute.shift();
+                const alteringAttributeName = formattedArgument.shift();
 
-                const alteringAttibute = alteringClass.attributes.find((attribute) => alteringAttributename === attribute.name);
+                const alteringAttribute = alteringClass.attributes.find((attribute) => alteringAttributeName === attribute.name);
 
-                if(alteringAttibute) {
-                    const alteringAttibuteIndex = alteringClass.attributes.indexOf(alteringAttibute);
+                if(alteringAttribute) {
+                    const alteringAttibuteIndex = alteringClass.attributes.indexOf(alteringAttribute);
 
-                    if(alteringAttribute[0] !== "-") {
-                        if(getKeyByValue(SUPPORTED_VISIBILITY, alteringAttribute[0])) {
-                            alteringAttibute.visibility = alteringAttribute[0];
+                    if(formattedArgument[0] !== "-") {
+                        if(getKeyByValue(SUPPORTED_VISIBILITY, formattedArgument[0])) {
+                            alteringAttribute.visibility = formattedArgument[0];
                         } else {
                             throw ERROR_COMMAND_SYNTAX;
                         }
                     }
 
-                    if(alteringAttribute[1] !== "-") {
-                        if(validateNameSpace(alteringAttribute[1])) {
-                            alteringAttibute.type = alteringAttribute[1];
+                    if(formattedArgument[1] !== "-") {
+                        if(validateNameSpace(formattedArgument[1])) {
+                            alteringAttribute.type = formattedArgument[1];
                         } else {
                             throw ERROR_COMMAND_SYNTAX;
                         }
                     }
 
-                    if(alteringAttribute[2] !== "-") {
-                        if(validateNameSpace(alteringAttribute[2])) {
-                            alteringAttibute.name = alteringAttribute[2];
+                    if(formattedArgument[2] !== "-") {
+                        if(validateNameSpace(formattedArgument[2])) {
+                            alteringAttribute.name = formattedArgument[2];
                         } else {
                             throw ERROR_COMMAND_SYNTAX;
                         }
                     }
 
-                    alteringClass.attributes.splice(alteringAttibuteIndex, 1, alteringAttibute);
+                    alteringClass.attributes.splice(alteringAttibuteIndex, 1, alteringAttribute);
                 } else {
                     throw ERROR_COMMAND_SYNTAX;
                 }
 
                 break;
 
+            default:
+                throw ERROR_COMMAND_SYNTAX;
+        }
+    });
+}
             default:
                 throw ERROR_COMMAND_SYNTAX;
         }
