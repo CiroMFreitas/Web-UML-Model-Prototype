05f03e9d2d0fd183e007303a464bd15c57ff5ddd
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-11-21 12:03:49 -0300

    altering class method and it's parameters implementation

--- src/Handlers/ClassEnityHandlers/AlterClassCommandHandler.js
+++ src/Handlers/ClassEnityHandlers/AlterClassCommandHandler.js
@@ -116,8 +116,166 @@ function alterAttributesHandler(alteringArguments, alteringClass) {
         }
     });
 }
+
+function alterMethodsHandler(alteringArguments, alteringClass) {
+    const formattedArguments = methodsFormatter(alteringArguments);
+    console.log(formattedArguments);
+    
+    formattedArguments.forEach((formattedArgument) => {
+        console.log(formattedArgument)
+        const argument = formattedArgument.argument.shift().toLowerCase();
+
+        switch(true) {
+            case SUPPORTED_ALTER_ARGUMENTS.add.includes(argument):
+                const newMethod = {
+                    visibility: "",
+                    type: "",
+                    name: "",
+                    parameters: []
+                }
+
+                switch(formattedArgument.argument.length) {
+                    case 3:
+                        if(getKeyByValue(SUPPORTED_VISIBILITY, formattedArgument.argument[0])) {
+                            newMethod.visibility = formattedArgument.argument[0];
+                            newMethod.type = validateNameSpace(formattedArgument.argument[1]);
+                            newMethod.name = validateNameSpace(formattedArgument.argument[2]);
+                        } else {
+                            throw ERROR_COMMAND_SYNTAX;
+                        }
+                        break;
+        
+                    case 2:
+                        newMethod.visibility = SUPPORTED_VISIBILITY.public[1];
+                        newMethod.type = validateNameSpace(formattedArgument.argument[0]);
+                        newMethod.name = validateNameSpace(formattedArgument.argument[1]);
+
+                        break;
+                    
+                    default:
+                        throw ERROR_COMMAND_SYNTAX;
+                }
+
+                newMethod.parameters = formattedArgument.paramenters.map((newParameter) => {
+                    return {
+                        type: validateNameSpace(newParameter[0]),
+                        name: validateNameSpace(newParameter[1])
+                    }
+                });
+
+                alteringClass.methods.push(newMethod);
+
+                break;
+            
+            case SUPPORTED_ALTER_ARGUMENTS.remove.includes(argument):
+                const removingMethodIndex = alteringClass.methods.findIndex((method) => formattedArgument.argument[0] === method.name);
+
+                alteringClass.methods.splice(removingMethodIndex, 1);
+
+                break;
+
+            case SUPPORTED_ALTER_ARGUMENTS.alter.includes(argument):
+                const alteringMethodName = formattedArgument.argument.shift();
+
+                const alteringMethod = alteringClass.methods.find((method) => alteringMethodName === method.name);
+
+                if(alteringMethod) {
+                    const alteringMethodIndex = alteringClass.methods.indexOf(alteringMethod);
+
+                    if(formattedArgument.argument[0] !== "-") {
+                        if(getKeyByValue(SUPPORTED_VISIBILITY, formattedArgument.argument[0])) {
+                            alteringMethod.visibility = formattedArgument.argument[0];
+                        } else {
+                            throw ERROR_COMMAND_SYNTAX;
+                        }
+                    }
+
+                    if(formattedArgument.argument[1] !== "-") {
+                        if(validateNameSpace(formattedArgument.argument[1])) {
+                            alteringMethod.type = formattedArgument.argument[1];
+                        } else {
+                            throw ERROR_COMMAND_SYNTAX;
+                        }
+                    }
+
+                    if(formattedArgument.argument[2] !== "-") {
+                        if(validateNameSpace(formattedArgument.argument[2])) {
+                            alteringMethod.name = formattedArgument.argument[2];
+                        } else {
+                            throw ERROR_COMMAND_SYNTAX;
+                        }
+                    }
+
+                    if(formattedArgument.paramenters.length > 0) {
+                        alteringMethod.parameters = alterMethodParametersHandler(formattedArgument.paramenters, alteringMethod.parameters);
+                    }
+
+                    alteringClass.methods.splice(alteringMethodIndex, 1, alteringMethod);
+                } else {
+                    throw ERROR_COMMAND_SYNTAX;
+                }
+
+                break;
+
             default:
                 throw ERROR_COMMAND_SYNTAX;
         }
     });
+}
+
+function alterMethodParametersHandler(alteringParamentersArguments, methodParameters) {
+    alteringParamentersArguments.forEach((alteringParamentersArgument) => {
+        const argument = alteringParamentersArgument.shift();
+    
+        switch(true) {
+            case SUPPORTED_ALTER_ARGUMENTS.add.includes(argument):
+                methodParameters.push({
+                    type: validateNameSpace(alteringParamentersArgument[0]),
+                    name: validateNameSpace(alteringParamentersArgument[1])
+                });
+
+                break;
+            
+            case SUPPORTED_ALTER_ARGUMENTS.remove.includes(argument):
+                const removingParameterIndex = methodParameters.findIndex((parameter) => alteringParamentersArgument[0] === parameter.name);
+
+                methodParameters.splice(removingParameterIndex, 1);
+
+                break;
+
+            case SUPPORTED_ALTER_ARGUMENTS.alter.includes(argument):
+                const alteringParameterName = alteringParamentersArgument.shift();
+
+                const alteringParameter = methodParameters.find((parameter) => alteringParameterName === parameter.name);
+
+                if(alteringParameter) {
+                    const alteringParameterIndex = methodParameters.indexOf(alteringParameter);
+
+                    if(alteringParamentersArgument[0] !== "-") {
+                        if(validateNameSpace(alteringParamentersArgument[0])) {
+                            alteringParameter.type = alteringParamentersArgument[0];
+                        } else {
+                            throw ERROR_COMMAND_SYNTAX;
+                        }
+                    }
+
+                    if(alteringParamentersArgument[1] !== "-") {
+                        if(validateNameSpace(alteringParamentersArgument[1])) {
+                            alteringParameter.name = alteringParamentersArgument[1];
+                        } else {
+                            throw ERROR_COMMAND_SYNTAX;
+                        }
+                    }
+
+                    methodParameters.splice(alteringParameterIndex, 1, alteringParameter);
+                }
+                    
+                break;
+            
+            default:
+                throw ERROR_COMMAND_SYNTAX;
+        }
+    });
+
+    return methodParameters;
 }
