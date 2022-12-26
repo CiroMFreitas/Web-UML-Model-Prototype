20e289a6589e8cc890fa72fdf2f79af1dd6f8139
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-12-26 13:45:56 -0300

    updated logic to use getArgumentsValueIndex function

--- src/Handlers/RelationshipEntityHandlers/CreateRelaionshipCommandHandler.js
+++ src/Handlers/RelationshipEntityHandlers/CreateRelaionshipCommandHandler.js
@@ -1,6 +1,6 @@
 import { ERROR_CLASS_DOES_NOT_EXISTS, ERROR_INSUFFICIENT_ARGUMENTS, ERROR_INVALID_CARDINALITY, ERROR_INVALID_RELATIONSHIP_TYPE } from "../../Utils/Errors";
 import { SUPPORTED_RELATIONSHIP_TYPES } from "../../Utils/SupportedKeyWords";
-import { getKeyByValue } from "../UtilityHandlers/DataHandler";
+import { getArgumentsValueIndex, getKeyByValue } from "../UtilityHandlers/DataHandler";
 import { upperCaseFirstLetter, validateNameSpace } from "../UtilityHandlers/StringHandler";
 
 /**
@@ -34,7 +34,7 @@ export default function createRelationshipCommandHandler(commandArray, classEnti
     }
 
     // Gets relationship or generates one if none is given
-    const nameIndex = commandArray.indexOf("-n") + 1;
+    const nameIndex = getArgumentsValueIndex(commandArray, "-n");
     let relationshipName;
     if(nameIndex === 0) {
         relationshipName = primaryClassName + relationshipType + secondaryClassName;
@@ -42,7 +42,7 @@ export default function createRelationshipCommandHandler(commandArray, classEnti
         relationshipName = commandArray[nameIndex];
     }
     
-    const cardinalityIndex = commandArray.indexOf("-c") + 1;
+    const cardinalityIndex = getArgumentsValueIndex(commandArray, "-c");
     let cardinality;
     if(cardinalityIndex !== 0) {
         cardinality = commandArray[cardinalityIndex].split(":");
