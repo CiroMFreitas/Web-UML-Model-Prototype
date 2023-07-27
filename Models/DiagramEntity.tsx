4d5272fbbe8545176fd7909d6b888c933156160e
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2023-07-27 13:09:34 -0300

    DiagramEntity defined

---
+++ Models/DiagramEntity.tsx
@@ -0,0 +1,61 @@
+import DomainObject from "./DomainObject";
+
+/**
+ * All entity which will be drawned on canvas, must inherit this class.
+ */
+export default abstract class DiagramEntity extends DomainObject {
+    private name: string;
+    private toDraw: boolean;
+
+    /**
+     * Sets class name and for it to not be drawn on creation.
+     * 
+     * @param name string
+     */
+    constructor(name: string) {
+        super();
+        this.name = name;
+        this.toDraw = false;
+    }
+
+    /**
+     * Sets entity's name.
+     * 
+     * @param name New name for entity.
+     */
+    public setName(name: string): void {
+        this.name = name;
+    }
+
+    /**
+     * Gets entity's name.
+     * 
+     * @returns Entity's name.
+     */
+    public getName(): string {
+        return this.name;
+    }
+
+    /**
+     * Returns a boolean determining whether or not should entity be drawn on diagram.
+     * 
+     * @returns If entity is to be drawn on diagram.
+     */
+    public isToDraw(): boolean {
+        return this.toDraw;
+    }
+
+    /**
+     * Set entity to be drawn on diagram.
+     */
+    public draw(): void {
+        this.toDraw = true;
+    }
+
+    /**
+     * Set entity to not be drawn on diagram.
+     */
+    public undraw(): void {
+        this.toDraw = false;
+    }
+}
