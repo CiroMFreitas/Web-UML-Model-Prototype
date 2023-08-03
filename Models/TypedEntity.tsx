bde1f8955741746a6c134be98cdb35935deb8ff6
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2023-08-03 14:59:57 -0300

    TypedEntity now checks if type is empty

--- Models/TypedEntity.tsx
+++ Models/TypedEntity.tsx
@@ -1,4 +1,8 @@
+import AppError from "./AppError";
 import DiagramEntity from "./DiagramEntity"
+import Feedback from "./Feedback";
+import LocalizationSnippet from "./LocalizationSnippet";
+import StringSnippet from "./StringSnippet";
 
 /**
  * All entities with types must inehrit this class
@@ -14,6 +18,16 @@ export default abstract class TypedEntity extends DiagramEntity {
      */
     constructor(name: string, type: string) {
         super(name);
+        if(type === "") {
+            const errorFeedback = new Feedback();
+            errorFeedback.addSnippet(new LocalizationSnippet("feedback.error.missing_type_for_entity.part_1"));
+            errorFeedback.addSnippet(new LocalizationSnippet("feedback.common."+this.constructor.name));
+            errorFeedback.addSnippet(new StringSnippet(" "+this.getName()));
+            errorFeedback.addSnippet(new LocalizationSnippet("feedback.error.missing_type_for_entity.part_2"));
+
+            throw new AppError(errorFeedback);
+        }
+
         this.validadeNamingString(type);
 
         this.type = type;
