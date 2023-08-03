9ec93d6b302ded287653d0a344e1ac2f7f55e94d
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2023-08-03 10:31:39 -0300

    now Classifiers checks if two attributes share names during it's creation

--- Models/Classifier.tsx
+++ Models/Classifier.tsx
@@ -26,9 +26,28 @@ export default class Classifier extends DiagramEntity {
         if(attributeArguments !== undefined) {
             attributeArguments.forEach((argument) => {
                 const newAttribute = new Attribute(argument);
+                this.isAttributeNameInUse(newAttribute.getName());
 
                 this.attributes.push(newAttribute);
             });
+        }
+    }
+
+    /**
+     * Checks if given name is already in use by an attribute, if true an error will be thrown.
+     * 
+     * @param attributeName Name to be checked.
+     */
+    private isAttributeNameInUse(attributeName: string): void {
+        const attributeExists = this.attributes.find((attribute) => attribute.getName() === attributeName);
+
+        if(attributeExists) {
+            const errorFeedback = new Feedback();
+            errorFeedback.addSnippet(new LocalizationSnippet("feedback.create.classifier.error.attribute_name_already_in_use.part_1"));
+            errorFeedback.addSnippet(new StringSnippet(attributeName))
+            errorFeedback.addSnippet(new LocalizationSnippet("feedback.create.classifier.error.attribute_name_already_in_use.part_2"));
+
+            throw new AppError(errorFeedback);
         }
     }
 }
