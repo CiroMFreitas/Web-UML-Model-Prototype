1a5a67722212ede4f569ae6204dddee704ae5073
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2023-08-03 13:52:06 -0300

    Parameter documentation

--- Models/Parameter.tsx
+++ Models/Parameter.tsx
@@ -1,6 +1,13 @@
 import TypedEntity from "./TypedEntity";
 
+/**
+ * Parameter of an method.
+ */
 export default class Parameter extends TypedEntity {
+    
+    /**
+     * Creates Parameter.
+     */
     constructor(name: string, type: string) {
         super(name, type);
     }
