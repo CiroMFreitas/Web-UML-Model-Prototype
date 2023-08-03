485e5ec8b1c3a53d40ed91d82121c05179d64ec6
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2023-08-03 13:54:02 -0300

    Classifier's stubs

--- Models/Classifier.tsx
+++ Models/Classifier.tsx
@@ -59,4 +59,22 @@ export default class Classifier extends DiagramEntity {
             throw new AppError(errorFeedback);
         }
     }
+
+    /**
+     * Stub.
+     * 
+     * @returns Stub.
+     */
+    public toText(): string {
+        return "";
+    }
+
+    /**
+     * Stub.
+     * 
+     * @returns Stub.
+     */
+    public toDiagram(): string {
+        return "";
+    }
 }
