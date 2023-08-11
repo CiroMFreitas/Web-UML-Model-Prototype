5d9e0783628b0aa5ed070f69b9329cf66d45cb81
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2023-08-11 16:47:45 -0300

    interpretAlterRelationship stub

--- Controller/AlterCommandInterpreter.tsx
+++ Controller/AlterCommandInterpreter.tsx
@@ -4,4 +4,8 @@ import IAlterRelationshipDTO from "../public/DTO/IAlterRelationshipDTO";
  * Class responsible for handling user's alter commands into DTOs.
  */
 export default class ReadCommandInterpreter {
+    public static interpretAlterRelationship(commandLine: string[]): IAlterRelationshipDTO {
+        
+        return { relationshipName: "" }
+    }
 }
