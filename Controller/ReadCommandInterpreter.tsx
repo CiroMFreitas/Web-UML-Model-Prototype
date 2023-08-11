1d6fa996314346574fce3161dbb96ba761a719d9
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2023-08-11 11:31:07 -0300

    interpretReadRelationship checks if read form is present

--- Controller/ReadCommandInterpreter.tsx
+++ Controller/ReadCommandInterpreter.tsx
@@ -1,3 +1,6 @@
+import AppError from "../Models/AppError";
+import Feedback from "../Models/Feedback";
+import LocalizationSnippet from "../Models/LocalizationSnippet";
 import IReadRelationshipByBetweenDTO from "../public/DTO/IReadRelationshipByBetweenDTO";
 import IReadRelationshipByNamed from "../public/DTO/IReadRelationshipByNamed";
 
@@ -12,7 +15,15 @@ export default class ReadCommandInterpreter {
      * @returns A DTO depending on whenever the 'named' or 'between' arguments were used.
      */
     public static interpretReadRelationship(commandLine: string[]): IReadRelationshipByNamed | IReadRelationshipByBetweenDTO {
+        const readForm = commandLine.shift();
 
+        // Checks if read form is present.
+        if((readForm !== undefined) || (readForm !== "")) {
+            const errorFeedback = new Feedback();
+            errorFeedback.addSnippet(new LocalizationSnippet("feedback.read.relationship.error.missing_read_form_argument"));
+
+            throw new AppError(errorFeedback);
+        }
 
         return { relationshipName: "" };
     }
