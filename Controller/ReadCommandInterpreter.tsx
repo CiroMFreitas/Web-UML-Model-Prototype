e9554cf4c55d32aabc2fa2c04326bb8c1a83dc17
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2023-08-11 10:58:38 -0300

    interpretReadRelationship fake

--- Controller/ReadCommandInterpreter.tsx
+++ Controller/ReadCommandInterpreter.tsx
@@ -1,5 +1,10 @@
+import IReadRelationshipDTO from "../public/DTO/IReadRelationshipDTO";
+
 /**
  * Class responsible for handling user's read commands into DTOs.
  */
 export default class ReadCommandInterpreter {
+    public static interpretReadRelationship(commandLine: string[]): IReadRelationshipDTO {
+        return { };
+    }
 }
