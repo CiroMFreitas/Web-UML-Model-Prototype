f2310c2128d2139564991dee8115f02f11c83f58
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2023-08-11 16:47:28 -0300

    Create AlterCommandInterpreter.tsx

---
+++ Controller/AlterCommandInterpreter.tsx
@@ -0,0 +1,7 @@
+import IAlterRelationshipDTO from "../public/DTO/IAlterRelationshipDTO";
+
+/**
+ * Class responsible for handling user's alter commands into DTOs.
+ */
+export default class ReadCommandInterpreter {
+}
