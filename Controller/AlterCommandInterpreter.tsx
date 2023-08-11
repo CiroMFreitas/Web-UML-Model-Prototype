53b1cf96167e559bc1ff8bb59bd4b12257a260b4
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2023-08-11 17:30:01 -0300

    ReadCommandInterpreter extends CommandInterpreter

--- Controller/AlterCommandInterpreter.tsx
+++ Controller/AlterCommandInterpreter.tsx
@@ -1,9 +1,10 @@
 import IAlterRelationshipDTO from "../public/DTO/IAlterRelationshipDTO";
+import CommandInterpreter from "./CommandInterpreter";
 
 /**
  * Class responsible for handling user's alter commands into DTOs.
  */
-export default class ReadCommandInterpreter {
+export default class ReadCommandInterpreter extends CommandInterpreter {
     public static interpretAlterRelationship(commandLine: string[]): IAlterRelationshipDTO {
         
         return { relationshipName: "" }
