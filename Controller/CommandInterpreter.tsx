0714942c5d881be06719407252fb6cc4ef2d509e
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2023-08-11 10:58:12 -0300

    CommandInterpreter specialized into ReadCommandInterpreter

--- Controller/CommandInterpreter.tsx
+++
@@ -1,6 +0,0 @@
-/**
- * Class responsible for handling user's commands into DTOs.
- */
-export default class CommandInterpreter {
-    
-}
---
+++ Controller/ReadCommandInterpreter.tsx
@@ -0,0 +1,5 @@
+/**
+ * Class responsible for handling user's read commands into DTOs.
+ */
+export default class ReadCommandInterpreter {
+}
