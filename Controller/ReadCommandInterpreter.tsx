a543ee3cde5befbb87902e9555bab9b09855f5f0
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2023-08-11 11:47:36 -0300

    interpretReadRelationship now checks if invalid read form was given

--- Controller/ReadCommandInterpreter.tsx
+++ Controller/ReadCommandInterpreter.tsx
@@ -1,6 +1,7 @@
 import AppError from "../Models/AppError";
 import Feedback from "../Models/Feedback";
 import LocalizationSnippet from "../Models/LocalizationSnippet";
+import StringSnippet from "../Models/StringSnippet";
 import IReadRelationshipByBetweenDTO from "../public/DTO/IReadRelationshipByBetweenDTO";
 import IReadRelationshipByNamed from "../public/DTO/IReadRelationshipByNamed";
 
@@ -24,7 +25,26 @@ export default class ReadCommandInterpreter {
 
             throw new AppError(errorFeedback);
         }
+        } else {
+            switch(readForm) {
+                case "named":
+                        return {
+                            relationshipName: ""
+                        };
 
-        return { relationshipName: "" };
+                case "between":
+                    return {
+                        sourceClassifierName: "",
+                        targetClassifierName: ""
+                    }
+
+                default:
+                    const errorFeedback = new Feedback();
+                    errorFeedback.addSnippet(new LocalizationSnippet("feedback.read.relationship.error.invalid_read_form_argument.part_1"));
+                    errorFeedback.addSnippet(new StringSnippet(readForm));
+                    errorFeedback.addSnippet(new LocalizationSnippet("feedback.read.relationship.error.invalid_read_form_argument.part_2"));
+                    
+                    throw new AppError(errorFeedback);
+            }
     }
 }
