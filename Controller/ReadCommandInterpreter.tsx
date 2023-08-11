ef40b99b44ad705bd908c88abeefa8a700d3cb37
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2023-08-11 11:20:39 -0300

    interpretReadRelationship documentation

--- Controller/ReadCommandInterpreter.tsx
+++ Controller/ReadCommandInterpreter.tsx
@@ -5,6 +5,12 @@ import IReadRelationshipByNamed from "../public/DTO/IReadRelationshipByNamed";
  * Class responsible for handling user's read commands into DTOs.
  */
 export default class ReadCommandInterpreter {
+    /**
+     * Handles a read relationship command line.
+     * 
+     * @param commandLine To be interpreted
+     * @returns A DTO depending on whenever the 'named' or 'between' arguments were used.
+     */
     public static interpretReadRelationship(commandLine: string[]): IReadRelationshipByNamed | IReadRelationshipByBetweenDTO {
 
 
