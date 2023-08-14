653e29e8ff7f8ac9ee29a9b98bf90a544e1de003
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2023-08-14 14:38:53 -0300

    Method constructor revision

--- Models/Method.tsx
+++ Models/Method.tsx
@@ -1,3 +1,4 @@
+import ICreateMethodDTO from "../public/DTO/ICreateMethodDTO";
 import AppError from "./AppError";
 import Feedback from "./Feedback";
 import LocalizationSnippet from "./LocalizationSnippet";
@@ -9,54 +10,23 @@ import VisibleEntity from "./VisibleEntity";
  * Classifier's methods.
  */
 export default class Method extends VisibleEntity {
-    private parameters: Parameter[];
+    private parameters = [] as Parameter[];
 
     /**
      * Creates a method.
      * 
      * @param methodArguments An array containing method's arguments and it's paramenter's.
      */
-    constructor(methodArguments: string[]) {
-        // Gets first method argument and possibly first parameter.
-        const argumentStart = methodArguments?.shift()?.split("(");
-        const splitArgument = argumentStart !== undefined ? argumentStart[0].split(":") : [""];
+    constructor(creationInstructions: ICreateMethodDTO) {
+        super(creationInstructions.name, creationInstructions.type, creationInstructions.visibility);
 
-        // Checks if enogh arguments ware given for method creation.
-        if(splitArgument.length === 3) {
-            super(splitArgument[1], splitArgument[2], splitArgument[0]);
-        } else if(splitArgument.length === 2) {
-            super(splitArgument[0], splitArgument[1]);
-        } else {
-            const errorFeedback = new Feedback();
-            if(splitArgument[0] === "") {
-                errorFeedback.addSnippet(new LocalizationSnippet("feedback.create.method.error.empty_method_argument"));
-            } else if(splitArgument.length === 1) {
-                errorFeedback.addSnippet(new LocalizationSnippet("feedback.create.method.error.invalid_method_arguments.part_1.too_few"));
-                errorFeedback.addSnippet(new StringSnippet(splitArgument.toString().replaceAll(",", ":")))
-            } else {
-                errorFeedback.addSnippet(new LocalizationSnippet("feedback.create.method.error.invalid_method_arguments.part_1.too_many"));
-                errorFeedback.addSnippet(new StringSnippet(splitArgument.toString().replaceAll(",", ":")))
-            }
-            errorFeedback.addSnippet(new LocalizationSnippet("feedback.create.method.error.invalid_method_arguments.part_2"));
-
-            throw new AppError(errorFeedback)
-        }
-
-        // Sets parameters with present.
-        this.parameters = [] as Parameter[];
-        if((argumentStart !== undefined) && (argumentStart[1] !== "")) {
-            const firstParameter = new Parameter(argumentStart[1].replace(")", "").replace(",", ""));
+        // Sets parameters if any arguments are present.
+        creationInstructions.parameters.forEach((parameterInstructions) => {
+            const firstParameter = new Parameter(parameterInstructions);
             this.isParameterNameInUse(firstParameter.getName());
 
             this.parameters.push(firstParameter)
-
-            methodArguments.forEach((parameterArgument) => {
-                const newParameter = new Parameter(parameterArgument.replace(")", "").replace(",", ""));
-                this.isParameterNameInUse(newParameter.getName());
-
-                this.parameters.push(newParameter);
-            });
-        }
+        })
     }
 
     /**
