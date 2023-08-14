3aabbe56b1bfaed3d06a5a9ca65bc1e28fa7dcb0
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2023-08-14 14:27:01 -0300

    Attribute constructor now expects a DTO

--- Models/Attribute.tsx
+++ Models/Attribute.tsx
@@ -1,4 +1,5 @@
 
+import ICreateAttributeDTO from "../public/DTO/ICreateAttributeDTO";
 import AppError from "./AppError";
 import Feedback from "./Feedback";
 import LocalizationSnippet from "./LocalizationSnippet";
@@ -13,30 +14,10 @@ export default class Attribute extends VisibleEntity {
     /**
      * Creates attribute.
      * 
-     * @param attributeArgument A string containg it's attributes divided by :.
+     * @param creationInstructions DTO if instructions for attribute creation.
      */
-    constructor(attributeArgument: string) {
-        // Checks if sufficient arguments were given for attribute creation.
-        const splitArgument = attributeArgument.split(":");
-        if(splitArgument.length === 3) {
-            super(splitArgument[1], splitArgument[2], splitArgument[0]);
-        } else if(splitArgument.length === 2) {
-            super(splitArgument[0], splitArgument[1]);
-        } else {
-            const errorFeedback = new Feedback();
-            if(splitArgument[0] === "") {
-                errorFeedback.addSnippet(new LocalizationSnippet("feedback.create.attribute.error.empty_attribute_argument"));
-            } else if(splitArgument.length === 1) {
-                errorFeedback.addSnippet(new LocalizationSnippet("feedback.create.attribute.error.invalid_attribute_arguments.part_1.too_few"));
-                errorFeedback.addSnippet(new StringSnippet(splitArgument.toString().replaceAll(",", ":")))
-            } else {
-                errorFeedback.addSnippet(new LocalizationSnippet("feedback.create.attribute.error.invalid_attribute_arguments.part_1.too_many"));
-                errorFeedback.addSnippet(new StringSnippet(splitArgument.toString().replaceAll(",", ":")))
-            }
-            errorFeedback.addSnippet(new LocalizationSnippet("feedback.create.attribute.error.invalid_attribute_arguments.part_2"));
-
-            throw new AppError(errorFeedback)
-        }
+    constructor(creationInstructions: ICreateAttributeDTO) {
+        super(creationInstructions.name, creationInstructions.type, creationInstructions.visibility);
     }
 
     /**
