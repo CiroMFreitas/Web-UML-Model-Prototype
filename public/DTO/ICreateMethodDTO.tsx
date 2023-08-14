751e23e2e308e089cbff85350008856d2770e4c3
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2023-08-14 11:34:24 -0300

    Classifier creation DTOs

---
+++ public/DTO/ICreateMethodDTO.tsx
@@ -0,0 +1,12 @@
+import ICreateParameterDTO from "./ICreateParameterDTO";
+
+/**
+ * Expects at least a name and type, visibility can be omitted, parameters expects to at least receivea empty
+ * array.
+ */
+export default interface ICreateMethodDTO {
+    visibility?: string;
+    name: string;
+    type: string;
+    parameters: ICreateParameterDTO[];
+}
