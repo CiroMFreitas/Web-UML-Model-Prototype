6371d15978de1f97a5b9ff82a49ede4b6f8bd1f0
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2023-08-18 16:06:48 -0300

    ImportCommandInterpreter interpretImportXML now slices xml content to be handled

--- Controller/ImportCommandInterpreter.tsx
+++ Controller/ImportCommandInterpreter.tsx
@@ -1,3 +1,6 @@
+import { parseString } from "xml2js";
+import ICreateClassifierDTO from "../public/DTO/ICreateClassifierDTO";
+import ICreateRelationshipDTO from "../public/DTO/ICreateRelationshipDTO";
 import IImportDiagramDTO from "../public/DTO/IImportDiagramDTO";
 import CommandInterpreter from "./CommandInterpreter";
 
@@ -13,9 +16,19 @@ export default class ImportCommandInterpreter extends CommandInterpreter {
      * @returns All classifiers and relationship imported from a xml file.
      */
     public static interpretImportXML(xmlImport: string): IImportDiagramDTO {
+        const diagramImportInstructions = {
+            classifiers: [] as ICreateClassifierDTO[],
+            relationships: [] as ICreateRelationshipDTO[]
+        };
+
+        parseString(xmlImport, (err, result) => {
+            // Get diagram content
+            const diagramContent = result.mxfile.diagram[0].mxGraphModel[0].root[0].mxCell.slice(2);
+        });
+
         return {
-            classifiersInstructions: [],
-            relationshipssInstructions: []
+            classifiersInstructions: diagramImportInstructions.classifiers,
+            relationshipssInstructions: diagramImportInstructions.relationships
         };
     }
 }
