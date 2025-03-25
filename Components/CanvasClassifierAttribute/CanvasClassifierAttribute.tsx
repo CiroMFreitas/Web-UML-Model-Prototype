be6ce1a86749b7b564be7a3eb52c798e76b80a7b
CiroMFreitas <cirofreitas1995@gmail.com>
2025-03-25 15:19:28 -0300

    CanvasClassifier componentization

---
+++ Components/CanvasClassifierAttribute/CanvasClassifierAttribute.tsx
@@ -0,0 +1,16 @@
+import IGetAttributeDTO from "../../public/DTO/IGetAttributeDTO";
+import "./CanvasClassifierAttribute.css";
+
+type Props = { attributeData: IGetAttributeDTO };
+
+export default function CanvasClassifierAttribute({ attributeData }: Props) {
+    return (
+      <div>
+        { 
+          attributeData.visibility + " " +
+          attributeData.name + ": " +
+          attributeData.type
+        }
+      </div>
+    )
+}
--- Components/ClassDiagramCanvas/ClassDiagramCanvas.tsx
+++ Components/ClassDiagramCanvas/ClassDiagramCanvas.tsx
@@ -1,76 +1,18 @@
 import React, { useContext } from 'react';
 import "./ClassDiagramCanvas.css";
 import CommandHandlerContext from '../../Contexts/CommandHandlerContext';
-import LocalizationSnippet from '../../Models/LocalizationSnippet';
+import CanvasClassifier from '../CanvasClassifier/CanvasClassifier';
 
 export default function ClassDiagramCanvas() {
   const commandHandlerContext = useContext(CommandHandlerContext);
   const toRenderEntityData = commandHandlerContext.getToRenderEntityData();
-
-  function translate(i18nContent: string): string {
-    return new LocalizationSnippet(i18nContent).toString()
-  }
 
   // Component
   switch(toRenderEntityData.entityType) {
     case "classifier":
       const classifierData = commandHandlerContext.getToRenderClassifier(toRenderEntityData.entityId);
       return (
-        <div id="ClassDiagramCanvas">
-          {/* Classifier type and name */}
-          <div>
-            { translate("ASCIIDiagram.classifier." + classifierData.classifierType) + " " + classifierData.name }
-          </div>
-
-          {/* Atributes */}
-          <div>
-            <div>
-              { translate("ASCIIDiagram.attribute.title") }
-            </div>
-            
-            <div>
-              {
-                classifierData.attributes.map((attribute) => {
-                  return (
-                    <div key={attribute.name}>
-                      { 
-                        attribute.visibility + " " +
-                        attribute.name + ": " +
-                        attribute.type
-                      }
-                    </div>
-                  )
-                })
-              }
-            </div>
-          </div>
-
-          {/* Methods */}
-          <div>
-            <div>
-              { translate("ASCIIDiagram.method.title") }
-            </div>
-
-            <div>
-              {
-                classifierData.methods.map((method) => {
-                  return (
-                    <div key={method.name}>
-                      {
-                        method.visibility + " " +
-                        method.name + "(" +
-                        method.parameters.map((parameter) => {
-                          return parameter.name + ": " + parameter.type
-                        }).toString().replaceAll(",", ", ") + "): " +
-                        method.type
-                      }
-                    </div>
-                  )
-                })
-              }
-            </div>
-          </div>
-        </div>
+        <CanvasClassifier classifierData={ classifierData } />
       );
 
     default:
