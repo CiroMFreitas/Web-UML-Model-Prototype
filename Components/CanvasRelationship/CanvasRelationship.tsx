66be380ed1d3c7b3e550f5f31adf7a8051ffd0e7
CiroMFreitas <cirofreitas1995@gmail.com>
2025-09-22 16:43:42 -0300

    savepoint

--- Components/CanvasRelationship/CanvasRelationship.tsx
+++ Components/CanvasRelationship/CanvasRelationship.tsx
@@ -1,21 +1,39 @@
 import translate from "../../Controller/Translate";
 import IGetRelationshipDTO from "../../public/DTO/IGetRelationshipDTO";
+import { SUPPORTED_RELATIONSHIP_TYPES } from "../../public/Utils/SupportedKeyWords";
 
 
 type Props = { relationshipData: IGetRelationshipDTO };
 
 export default function CanvasRelationship({ relationshipData }: Props) {
+    const relationshipType = SUPPORTED_RELATIONSHIP_TYPES.find((relationshipType) => relationshipType.name === relationshipData.relationshipType);
+
     return (
         <div id="ClassDiagramCanvas">
             {/* Classifier type and name */}
             <div>
                 {
                     translate("ASCIIDiagram.relationship.title_single") +
-                    translate("feedback.common.relationship_type." + relationshipData.relationshipType) +
-                    translate("ASCIIDiagram.relationship.between") +
-                    relationshipData.sourceClassifierName +
-                    translate("ASCIIDiagram.relationship.and") +
+                    relationshipData.relationshipName
+                    
+                }
+            </div>
+
+            -----------
+
+            <div>
+                { 
+                    relationshipData.sourceClassifierName + " " +
+                    relationshipType?.ascii + " " +
                     relationshipData.targetClassifierName
+                }
+            </div>
+
+            -----------
+
+            <div>
+                { 
+                    
                 }
             </div>
         </div>
